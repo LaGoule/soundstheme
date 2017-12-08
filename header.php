e7e3d2e98fc16dcea67353e3107fa19beb3749a7
LaGoule <damien.beuchat@gmail.com>
2017-12-08 18:38:48 +0100

    Wrapper
    -Site wrappé
    -Début de site-header
    -Mise en place de la structure

--- header.php
+++ header.php
@@ -21,6 +21,8 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div id="site-wrapper">
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'soundstheme' ); ?></a>
 
