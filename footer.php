96e600537baff72d955ad1ef881870377c2fe646
LaGoule <damien.beuchat@gmail.com>
2017-12-23 00:30:33 +0100

    Bitmap shadow
    -bitmap shadows
    -clean footer
    -post-it csss
    -minor fix

--- footer.php
+++ footer.php
@@ -14,16 +14,8 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'soundstheme' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'soundstheme' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'soundstheme' ), 'soundstheme', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
+		<div class="site-info" style="padding:10px;">
+			<p>Un footer</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #site-wrapper -->
