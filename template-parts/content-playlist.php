54c30da6ca49a061226382affe926e1b092cf5ee
LaGoule <damien.beuchat@gmail.com>
2018-01-13 20:47:34 +0100

    Playlist content
    -pas de mise en forme

--- template-parts/content-playlist.php
+++ template-parts/content-playlist.php
@@ -19,15 +19,26 @@ if( have_rows('track') ):
 		echo '<ul>';
  		// loop through the rows of data
     while ( have_rows('track') ) : the_row();
+				$sfid = get_sub_field('image');
+				$aname = get_sub_field('artist');
+				$tname = get_sub_field('trackname');
+				$desc = get_sub_field('description');
+				$ytlink = get_sub_field('youtubelink');
+				$bclink = get_sub_field('bandcamplink');
+
 				echo '<li class="piste">';
-
-        // display a sub field value
-				//if(get_sub_field('image')){
-					//$sfid = the_sub_field('image');
-					//wp_get_attachment_image( $sfid, array('100', '100'), "", array( "class" => "cover" ) );
-				//}
-
-        // display a sub field value
+        // Cover
+				if($sfid){echo wp_get_attachment_image( $sfid, 'thumbnail', "", array( "class" => "cover" ) );}
+        // Artiste
+				if($aname){echo '<p>'.$aname.'</p>';}
+        // Track
+				if($tname){echo '<p>'.$tname.'</p>';}
+        // Description
+				if($desc){echo '<p>'.$desc.'</p>';}
+        // Youtube Link
+				if($ytlink){echo '<p>'.$ytlink.'</p>';}
+        // Youtube Link
+				if($bclink){echo '<p>'.$bclink.'</p>';}
 
 				echo '</li>';
 
