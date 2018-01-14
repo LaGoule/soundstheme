58f210afa482ddd03a5e59b08520f2ad698047c0
LaGoule <damien.beuchat@gmail.com>
2018-01-14 07:04:38 +0100

    Playlist css
    -Ajout règles css pour playlist
    -Reglage bug marge à gauche (body)
    -Reglage bug padding contact inputs

--- template-parts/content-playlist.php
+++ template-parts/content-playlist.php
@@ -9,16 +9,15 @@
 
 ?>
 
-
-
 <?php
 
 // check if the repeater field has rows of data
 if( have_rows('track') ):
 
-		echo '<ul>';
+		echo '<ul class="the-list">';
  		// loop through the rows of data
     while ( have_rows('track') ) : the_row();
+				//ACF Variables
 				$sfid = get_sub_field('image');
 				$aname = get_sub_field('artist');
 				$tname = get_sub_field('trackname');
@@ -26,20 +25,25 @@ if( have_rows('track') ):
 				$ytlink = get_sub_field('youtubelink');
 				$bclink = get_sub_field('bandcamplink');
 
-				echo '<li class="piste">';
-        // Cover
-				if($sfid){echo wp_get_attachment_image( $sfid, 'thumbnail', "", array( "class" => "cover" ) );}
-        // Artiste
-				if($aname){echo '<p>'.$aname.'</p>';}
-        // Track
-				if($tname){echo '<p>'.$tname.'</p>';}
-        // Description
-				if($desc){echo '<p>'.$desc.'</p>';}
-        // Youtube Link
-				if($ytlink){echo '<p>'.$ytlink.'</p>';}
-        // Youtube Link
-				if($bclink){echo '<p>'.$bclink.'</p>';}
+				//Nouvelle track dans la liste
+				echo '<li class="the-track">';
+	        // Cover
+					if($sfid){echo wp_get_attachment_image( $sfid, 'thumbnail', "", array( "class" => "cover" ) );}
 
+					echo '<div class="textwrap"><div class="textbox">';
+		        // Artiste
+						if($aname){echo '<p class="artist">'.$aname.'</p>';}
+		        // Track
+						if($tname){echo '<p class="trackname">'.$tname.'</p>';}
+		        // Description
+						if($desc){echo '<p class="description">'.$desc.'</p>';}
+
+						echo '<div class="linkbox">';
+			        // Youtube Link
+							if($ytlink){echo '<a href='.$ytlink.' class="youtubelink">Youtube</a>';}
+			        // Youtube Link
+							if($bclink){echo '<a href='.$bclink.' class="bandcamplink">Bandcamp</a>';}
+					echo '</div></div></div>';
 				echo '</li>';
 
     endwhile;
