b28c5292c89427a05fd7859e51258d8a78610534
LaGoule <damien.beuchat@gmail.com>
2018-01-27 14:31:46 +0100

    Backgrounds placements
    -Events css minor modif
    -Eventsoff no longer displayed as opacity 0

--- single-page-theme.php
+++ single-page-theme.php
@@ -40,7 +40,7 @@ $wpb_all_query = new WP_Query(array('post_type'=>'post', 'cat'=>8, 'post_status'
 
 <?php //Event
 // the query
-$wpb_all_query = new WP_Query(array('post_type'=>'post', 'cat'=>'6,7', 'post_status'=>'publish', 'posts_per_page'=>1)); ?>
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'cat'=>'6', 'post_status'=>'publish', 'posts_per_page'=>1)); ?>
 <?php if ( $wpb_all_query->have_posts() ) : ?>
     <!-- the loop -->
     <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
